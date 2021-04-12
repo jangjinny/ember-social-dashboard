3b9d0f3cc0f0b73a5a7fa3cca259089760d6efb2
Jinny Jang <jangjinny21@gmail.com>
2021-04-11 22:02:03 -0700

    create functiont to get total subscriber count

---
+++ api/youtube/getSubscriberCount.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const got = require('got');
+const apiKey = process.env.YOUTUBE_API_KEY;
+const channelId = 'UCAdjKaEb4cki7NyKojOGgrA'; // Cate's channel ID
+
+const getSubcriberCount = async () => {
+
+  const response = await got(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${channelId}&key=${apiKey}`);
+
+  const results = JSON.parse(response.body).items[0].statistics;
+  const subscriberCount = results.subscriberCount;
+
+  console.log("This is the subscriber count:", subscriberCount);
+};
+
+getSubcriberCount();
+
