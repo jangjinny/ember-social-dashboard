0f4222a15f43656e140f77a932aecdb3ee4163cf
Jinny Jang <jangjinny21@gmail.com>
2021-04-11 22:05:00 -0700

    get artist_id from params

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class DashboardRoute extends Route {
-  model(params) {
+  async model(params) {
     const {
       artist_id
     } = params;
