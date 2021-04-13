e0dd10936c306b7aefde81a0c36165778ee462f1
Jinny Jang <jangjinny21@gmail.com>
2021-04-12 18:52:39 -0700

    insert artist data in json format

---
+++ app/controllers/dashboard.js
@@ -0,0 +1,9 @@
+import Controller from '@ember/controller';
+import EmberObject, { set } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { equal } from '@ember/object/computed';
+
+export default class DashboardController extends Controller {
+
+}
+
---
+++ app/data/artist.js
@@ -0,0 +1,39 @@
+export const artist = {
+  "name": "IU",
+  "description": "A South Korean singer-songwriter and actress",
+  "data": [
+    { "platform": "youtube",
+    "followers": "98.9k",
+    "mostViewedVideo": {
+      "title": "Palette",
+      "album": "https://upload.wikimedia.org/wikipedia/en/b/b6/IU_Palette_final.jpg",
+      "views": 163
+    },
+    "mostRecentSong": {
+      "title": "Lilac",
+      "album": "https://bandwagon-gig-finder.s3.amazonaws.com/system/tinymce/image/file/2156/content_mceu_47980652711616659911799.jpg",
+      "plays": 93.2
+    }
+    },
+    {
+      "platform": "instagram",
+      "followers": "12.9m",
+      "post": "https://kpopchingu.com/wp-content/uploads/2020/06/97-1024x1024.jpg",
+      "likes": "3.1m",
+      "comments": "14.1k"
+    },
+    {
+      "platform": "spotify",
+      "followers": "25.2m",
+    },
+    {
+      "platform": "tiktok",
+      "followers": "10.2m",
+      "totalLikes": 2100042
+    },
+    {
+      "platform": "twitter",
+      "followers": "78.3k"
+    }
+  ]
+}
