2a70dff64f604765b63689444cb2200be784b075
Jinny Jang <jangjinny21@gmail.com>
2021-04-12 18:49:08 -0700

    return artist data

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,10 +1,13 @@
 import Route from '@ember/routing/route';
+import { artist } from '../data/artist';
 
 export default class DashboardRoute extends Route {
   async model(params) {
     const {
       artist_id
     } = params;
-    return artist_id;
+
+    console.log(artist)
+    return artist;
   }
 }
